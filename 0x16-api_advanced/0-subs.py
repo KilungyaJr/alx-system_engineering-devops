c69ad8b8ac25f48e4086a2f04f00f582643fe5fe
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-28 14:58:14 +0300

    added docstring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-
+"""Queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+"""
 import requests
 
 
