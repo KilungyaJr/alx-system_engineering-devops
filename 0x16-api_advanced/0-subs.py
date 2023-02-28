90d69b53fa4f38217eda1c38b139b0441bba9dd8
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-28 15:06:34 +0300

    handled 302 and 404 status codes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and
+"""
+Queries the Reddit API and
 returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given,
-the function should return 0.
 """
 import requests
 
@@ -16,10 +13,14 @@ def number_of_subscribers(subreddit):
     If an invalid subreddit is given, the function returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Python/requests'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
+    elif response.status_code == 302:
+        return 0
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
