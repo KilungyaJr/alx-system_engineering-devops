418106918e80bd96ee7ea618dba1fccfd17ad4a0
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-28 14:55:59 +0300

    how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
